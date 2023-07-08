24461753aaffa5c094d2fa048bcb6dee36c8f3f2
Shreyas sharma <shs30102@gmail.com>
2023-07-09 00:22:33 +0530

    Add existing project files to Git

--- main.py
+++ main.py
@@ -279,8 +279,19 @@ def getData():
 
 if __name__ == "__main__":
     from waitress import serve
-    serve(app, host="0.0.0.0", port=5000)
+    #serve(app, host="0.0.0.0", port=5000)
     app.run(host="0.0.0.0", port=5000)
     # newmain()
     # main()
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	return 'Hello World!'
+
+if __name__ == "__main__":
+	app.run()
     
